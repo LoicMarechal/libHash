0106c3ecbe79a01312d05025ad750c7b310ab5ce
Loïc Maréchal <loic.marechal@inria.fr>
2024-01-05 10:32:22 +0100

    Protected multiple include of libhash.h

--- sources/libhash.h
+++ sources/libhash.h
@@ -2,16 +2,20 @@
 
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
-/*                               LIBHASH 1.40                                 */
+/*                               LIBHASH 1.41                                 */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*   Description:       various operations on hash tables                     */
 /*   Author:            Loic MARECHAL                                         */
 /*   Creation date:     sep 25 2015                                           */
-/*   Last modification: jun 19 2019                                           */
+/*   Last modification: jan 04 2024                                           */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+
+#ifndef LIBHASH1_H
+#define LIBHASH1_H
 
 
 /*----------------------------------------------------------------------------*/
@@ -49,3 +53,5 @@ itg      hsh_FreeTable  (int64_t);
 itg      hsh_AddItem    (int64_t, itg, itg, itg, itg);
 itg      hsh_DeleteItem (int64_t, itg, itg, itg);
 itg      hsh_GetItem    (int64_t, itg, itg, itg, itg *, itg **, char **);
+
+#endif
